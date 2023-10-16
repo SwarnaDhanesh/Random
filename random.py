a15230156e2e526dcc53aa64623688e2aa77dc9a
SwarnaDhanesh <136910060+SwarnaDhanesh@users.noreply.github.com>
2023-10-17 00:14:02 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+array=np.random.rand(10,10)
+min_value=array.min()
+max_value=array.max()
+print("Minimum Value:",min_value)
+print("Maximum Value:",max_value)
+
